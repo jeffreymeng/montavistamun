95f62d9984ce838eed7cc4e5bca6c7839df360b4
Jeffrey Meng <jeffkmeng@gmail.com>
2020-10-30 19:13:35 -0700

    feat: re-open registation temporarily

--- src/pages/conferences/sfmun/register.tsx
+++ src/pages/conferences/sfmun/register.tsx
@@ -26,40 +26,40 @@ export default function AboutPage({
 	useRequireLogin();
 	const firebase = useFirebase();
 	const { user, loading: userLoading } = useContext(AuthContext);
-	// TODO: delete this file -- for now, keep as a template
-	const [registered, setRegistered] = useState("loading");
-	React.useEffect(() => {
-		if (!firebase || !user) return;
-		firebase
-			.firestore()
-			.collection("registration")
-			.doc(user.uid)
-			.get()
-			.then((snapshot) => snapshot.data())
-			.then((data) => setRegistered(data?.confirm?.sfmunConfirmed));
-	}, [firebase, user]);
-	return (
-		<Layout title={"SFMUN"}>
-			<Header
-				backgroundImage={headerImage}
-				title={"SFMUN Registration is Now Closed"}
-			>
-				Your Registration Status:{" "}
-				{registered === "loading"
-					? "Loading..."
-					: registered
-					? "Successfully Registered"
-					: "Not Registered"}
-			</Header>
-			<Main>
-				If you have any questions, feel free to email us at{" "}
-				<a href="mailto:montavistamun@gmail.com" className="link">
-					montavistamun@gmail.com
-				</a>
-				.
-			</Main>
-		</Layout>
-	);
+	// // TODO: delete this file -- for now, keep as a template
+	// const [registered, setRegistered] = useState("loading");
+	// React.useEffect(() => {
+	// 	if (!firebase || !user) return;
+	// 	firebase
+	// 		.firestore()
+	// 		.collection("registration")
+	// 		.doc(user.uid)
+	// 		.get()
+	// 		.then((snapshot) => snapshot.data())
+	// 		.then((data) => setRegistered(data?.confirm?.sfmunConfirmed));
+	// }, [firebase, user]);
+	// return (
+	// 	<Layout title={"SFMUN"}>
+	// 		<Header
+	// 			backgroundImage={headerImage}
+	// 			title={"SFMUN Registration is Now Closed"}
+	// 		>
+	// 			Your Registration Status:{" "}
+	// 			{registered === "loading"
+	// 				? "Loading..."
+	// 				: registered
+	// 				? "Successfully Registered"
+	// 				: "Not Registered"}
+	// 		</Header>
+	// 		<Main>
+	// 			If you have any questions, feel free to email us at{" "}
+	// 			<a href="mailto:montavistamun@gmail.com" className="link">
+	// 				montavistamun@gmail.com
+	// 			</a>
+	// 			.
+	// 		</Main>
+	// 	</Layout>
+	// );
 
 	const [step, setStep] = useState(0);
 	const [stepHasChanges, setStepHasChanges] = useState(false);
