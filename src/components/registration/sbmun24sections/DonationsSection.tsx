4e4b025a86820476fee36e115282c7b4695ed090
Ethan <ekellogg2020@gmail.com>
2024-02-27 17:41:56 -0800

    edited the sbmun registration to reflect no fee

--- src/components/registration/sbmun24sections/DonationsSection.tsx
+++ src/components/registration/sbmun24sections/DonationsSection.tsx
@@ -16,7 +16,6 @@ interface DonationForms {
 interface ConfirmationData {
 	sbmun24Confirmed: true;
 }
-//TODO: Replace example.com with actual donation link, find actual spreadsheet ID
 export default function DonationsSection({
 	data,
 	setStepHasChanges,
@@ -58,138 +57,10 @@ export default function DonationsSection({
 					Hang in there — you’re almost done!
 				</h3>
 				<p className="mt-2">
-					Before you submit, we just have one last step. To cover the
-					fees charged by SBMUN, we are requesting that you include a
-					donation of <b>$25</b> with your registration. Your
-					donations will go directly towards covering conference fees
-					and making this conference possible.
+                    Because Monta Vista MUN is cohosting SBMUN, you don't have to pay a delegation fee!
+                    To submit your registration for SBMUN, please click the button below.
+                    Note that after submitting you cannot go back and change the details of your registration.
 				</p>
-				{data.forms?.sbmun24DonationOptOut && (
-					<>
-						<p className="mt-2">
-							You've indicated that you are financially unable to
-							donate, or do not wish to do so, and that{" "}
-							<b>
-								you've talked to Mr Pelkey about your
-								situation
-							</b>
-							. You may now move on to the next step by clicking
-							the continue button below.
-						</p>
-						<button
-							className={cx(
-								"mt-4 py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white shadow-sm",
-								submitting
-									? "bg-indigo-300"
-									: "bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:ring-blue-500 active:bg-indigo-600 transition duration-150 ease-in-out"
-							)}
-							disabled={skipping}
-							onClick={(e) => {
-								setSkipping(true);
-								handleUpdateData("forms", {
-									sbmun24DonationOptOut: false,
-								}).then(() => setSkipping(false));
-							}}
-						>
-							{skipping ? "Cancelling..." : "Cancel"}
-						</button>
-					</>
-				)}
-				
-				{!data.forms?.sbmun24DonationOptOut && (
-					<>
-						<p className="mt-2">
-							If you are financially unable to donate, or do not
-							wish to do so,{" "}
-							<b>
-								please contact Mr. Pelkey during tutorial in room C107
-							</b>
-							, then{" "}
-							<a
-								className={"link"}
-								onClick={(e) => {
-									e.preventDefault();
-									if (skipping) return;
-									setSkipping(true);
-									handleUpdateData("forms", {
-										sbmun24DonationOptOut: true,
-									}).then(() => setSkipping(false));
-								}}
-							>
-								{skipping
-									? "skipping donating..."
-									: "click here to skip donating"}
-							</a>
-							.
-						</p>
-
-						<h3 className="text-lg leading-6 font-medium text-gray-900 mt-6 mb-2">
-							How to Donate
-						</h3>
-
-						<ul className="list-decimal ml-8">
-							<li>
-								Head to Monta Vista’s online Student Store and
-								find our sbmun conference donation{" "}
-								<a
-									target={"_blank"}
-									rel={"noopener noreferrer"}
-									href={
-										"https://montavista.myschoolcentral.com/"
-									}
-									className={"link"}
-								>
-									here
-								</a>
-								.
-							</li>
-							<li>Fill out the fields listed and pay.</li>
-							<li>
-								Make sure you save a copy of your receipt
-								(screenshot or print &gt; save as PDF the
-								confirmation page).
-							</li>
-							<li>Upload your receipt below.</li>
-						</ul>
-						<p className={"mt-4"}>
-							Unfortunately, we are unable to process refunds with
-							the online system. Please confirm you can attend the
-							conference (<b>April 6, 2024</b>) before
-							donating!
-						</p>
-
-						<div className={"mt-4"}>
-							<FormUpload
-								onInstanceChange={(instance) =>
-									setUploadInstance(instance)
-								}
-								user={user}
-								file={receipt}
-								setFile={setReceipt}
-								uploading={uploading}
-								setUploading={setUploading}
-								fieldName={"sbmun24Donation"}
-								data={data}
-								allowImagePreview
-								acceptedFileTypes={[
-									"application/pdf",
-									"image/*",
-								]}
-								labelFileTypeNotAllowed={
-									"That's not an image or PDF!"
-								}
-								fileValidateTypeLabelExpectedTypes={
-									"You may only upload images or pdfs"
-								}
-								labelIdle={
-									"Drag a image / pdf here or click to browse"
-								}
-								handleUpdateData={handleUpdateData}
-								conferenceName={"sbmun24"}
-							/>
-						</div>
-					</>
-				)}
 			</div>
 			<div className="px-4 py-3 bg-gray-50 text-right sm:px-6 flex justify-between">
 				<span className="inline-flex rounded-md shadow-sm">
@@ -210,22 +81,17 @@ export default function DonationsSection({
 				<button
 					type={"button"}
 					onClick={() => {
-						if (
-							!data?.forms?.sbmun24Donation &&
-							!data?.forms?.sbmun24DonationOptOut
-						) {
-							if (!uploadInstance) return;
-							setUploading(true);
-							uploadInstance.processFile();
-							return;
-						}
 						if (data.confirm?.sbmun24Confirmed) {
 							setStep(5);
 							return;
 						}
 						setSubmitting(true);
+						handleUpdateData("forms", {
+							sbmun24DonationOptOut: true,
+						}).then(() => {
 						handleUpdateData("confirm", {
 							sbmun24Confirmed: true,
+						})
 						})
 							.then(() => {
 								if (!firebase) {
@@ -265,32 +131,18 @@ export default function DonationsSection({
 					disabled={
 						uploading ||
 						submitting ||
-						skipping ||
-						((!receipt || receipt.length == 0) &&
-							!data?.forms?.sbmun24DonationOptOut)
+						skipping
 					}
 					className={cx(
 						"ml-4 py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white shadow-sm",
 						uploading ||
 							submitting ||
-							skipping ||
-							((!receipt || receipt.length == 0) &&
-								!data?.forms?.sbmun24DonationOptOut)
+							skipping
 							? "bg-indigo-300"
 							: "bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:ring-blue-500 active:bg-indigo-600 transition duration-150 ease-in-out"
 					)}
 				>
-					{uploading
-						? "Uploading..."
-						: (!receipt || receipt.length == 0) &&
-						  !data?.forms?.sbmun24DonationOptOut
-						? "Upload all forms to finish registration"
-						: !data?.forms?.sbmun24Donation &&
-						  !data?.forms?.sbmun24DonationOptOut
-						? "Upload"
-						: !data.confirm?.sbmun24Confirmed
-						? "Finish Registration"
-						: "Continue"}
+					Finish Registration
 				</button>
 			</div>
 		</div>
