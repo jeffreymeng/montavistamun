b98193fd3f51fcc9de3092e9c46643a7bf743e1b
Ethan <ekellogg2020@gmail.com>
2024-09-13 20:48:36 -0700

    rename webpack.functions.js

--- src/netlify-functions/config/webpack.functions.js
+++
@@ -1,7 +0,0 @@
-// solves problem from https://github.com/netlify/netlify-lambda/issues/112
-
-const nodeExternals = require("webpack-node-externals");
-
-module.exports = {
-	externals: [nodeExternals()],
-};
